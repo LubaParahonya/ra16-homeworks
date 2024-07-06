1fa4d68df15f8f97d0ef709002800845b4143d39
Lubasha <parakhonya1@yandex.ru>
2024-07-06 18:21:49 +0300

    className

--- events-state/my-react-app/src/App.jsx
+++ events-state/my-react-app/src/App.jsx
@@ -9,91 +9,17 @@ import { v4 as uuid } from "uuid";
 class App extends Component{
   constructor(props){
     super(props)
-    this.state = {
-      projects : [{
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/mon.jpg",
-        category: "Business Cards"
-      }, {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
-        category: "Websites"
-      }, {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
-        category: "Websites"
-      }, {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/codystretch.jpg",
-        category: "Websites"
-      }, {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_003.jpg",
-        category: "Business Cards"
-      }, {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290.png",
-        category: "Websites"
-      }, {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
-        category: "Websites"
-      }, {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
-        category: "Business Cards"
-      }, {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_1.png",
-        category: "Websites"
-      }, {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_2.png",
-        category: "Flayers"
-      }, {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/the_ninetys_brand.jpg",
-        category: "Websites"
-      }, {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/dia.jpg",
-        category: "Business Cards"
-      }, {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197.jpg",
-        category: "Websites"
-      }, {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
-        category: "Websites"
-      }, {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
-        category: "Business Cards"
-      }, {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197_1.jpg",
-        category: "Websites"
-      }, {
-        img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_3.png",
-        category: "Flayers"
-      }],
-      currentProject: [],
-      selected: 'All'
     }
-    this.state.currentProject = this.state.projects
-    this.chooseProjects = this.chooseProjects.bind(this)
     
-  }
-
-  
-
   render(){
     return (
       <>
-      <Toolbar chooseProjects={this.chooseProjects}/>
-      <div className='imgStyle'>{this.state.currentProject.map(el => (<img key={uuid()} src={el.img}></img>))}</div>
+      <Toolbar />
       </>
       
     )
   }
 
-  chooseProjects(category){
-    if(category === 'All'){
-      this.setState({
-        currentProject: this.state.projects
-      })
-      return
-    }
-    this.setState({
-      currentProject: this.state.projects.filter(el => el.category === category)
-    })
-
-  }
 
 }
 
--- events-state/my-react-app/src/Toolbar/Toolbar.jsx
+++ events-state/my-react-app/src/Toolbar/Toolbar.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import App from '../App'
-import { v4 as uuid } from "uuid";
+import { v4 as uuid } from 'uuid';
 
 
 
@@ -8,15 +8,91 @@ class Toolbar extends Component{
     constructor(props){
         super(props)
         this.state = {
-            filters: ['All', 'Websites', 'Flayers', 'Business Cards'],
-        
-        } 
+          currentProject: [],
+          selected: 'All'
+        }
+        this.projects = [{
+          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/mon.jpg",
+          category: "Business Cards"
+        }, {
+          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
+          category: "Websites"
+        }, {
+          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
+          category: "Websites"
+        }, {
+          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/codystretch.jpg",
+          category: "Websites"
+        }, {
+          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_003.jpg",
+          category: "Business Cards"
+        }, {
+          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290.png",
+          category: "Websites"
+        }, {
+          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
+          category: "Websites"
+        }, {
+          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
+          category: "Business Cards"
+        }, {
+          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_1.png",
+          category: "Websites"
+        }, {
+          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_2.png",
+          category: "Flayers"
+        }, {
+          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/the_ninetys_brand.jpg",
+          category: "Websites"
+        }, {
+          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/dia.jpg",
+          category: "Business Cards"
+        }, {
+          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197.jpg",
+          category: "Websites"
+        }, {
+          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
+          category: "Websites"
+        }, {
+          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
+          category: "Business Cards"
+        }, {
+          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197_1.jpg",
+          category: "Websites"
+        }, {
+          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_3.png",
+          category: "Flayers"
+        }];
+        this.arr = ['All'];
+        this.projects.map(el => this.arr.push(el.category)) 
+        this.filters = this.arr.filter((item, index) => {
+          return this.arr.indexOf(item) === index
+      });
+      this.state.currentProject = this.projects
+      this.chooseProjects = this.chooseProjects.bind(this)
 
+      
     }
     render(){
         return (<>
-       {this.state.filters.map(el => (<div className='filters' key={uuid()} onClick={()=> this.props.chooseProjects(el)}>{el}</div>))}
+  {this.filters.map(el => (<div className={el === this.state.selected? 'btn-activ': 'filters' } key={uuid()} onClick={()=> this.chooseProjects(el)}>{el}</div>))}
+  <div className='imgStyle'>{this.state.currentProject?.map(el => (<img key={uuid()} src={el.img}></img>))}</div>
         </>)
+    }
+
+    chooseProjects(category){
+      if(category === 'All'){
+        this.setState({
+          currentProject: this.projects,
+          selected : category
+        })
+        return
+      }
+      this.setState({
+        currentProject: this.projects.filter(el => el.category === category),
+        selected : category
+      })
+  
     }
 }
 
